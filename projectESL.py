5c172f3c3e5649cd5ddd533fe23b87ab71fe1518
Tim Hermans <timthjhermans@gmail.com>
2023-12-07 14:53:28 +0100

    added several features

--- projectESL.py
+++ projectESL.py
@@ -5,156 +5,156 @@ Created on Thu Nov 23 10:51:03 2023
 
 @author: timhermans
 """
+import sys
+sys.path.insert(2, '/Users/timhermans/Documents/GitHub/projectESL/esl_analysis')
+sys.path.insert(3, '/Users/timhermans/Documents/GitHub/projectESL/projecting')
+
 import os
 import yaml
 import xarray as xr
 import numpy as np
-import numpy.matlib
-import sys
-sys.path.insert(1, '/Users/timhermans/Documents/GitHub/projectESL/data_preprocessing')
-sys.path.insert(2, '/Users/timhermans/Documents/GitHub/projectESL/esl_analysis')
-sys.path.insert(3, '/Users/timhermans/Documents/GitHub/projectESL/projecting')
 from utils import mindist
-from open_GESLA import extract_GESLA2_locations, extract_GESLA3_locations, open_GESLA2_files, open_GESLA3_files
-from preprocess_GESLA import detrend_gesla_dfs, deseasonalize_gesla_dfs, subtract_amean_from_gesla_dfs
 from multivariate_normal_gpd_samples_from_covmat import multivariate_normal_gpd_samples_from_covmat
 from compute_AF import compute_AF
-from analyze_GESLA_ESLs import pot_extremes_from_gesla_dfs, fit_gpd_to_gesla_extremes
 from GPD_Z_from_F import get_return_curve
+import pandas as pd
+
 with open('config/config.yml', 'r') as f: #load config
     cfg = yaml.safe_load(f)
 
-#settings (to be included in a config file)
 f= 10**np.linspace(-6,2,num=2001)
 f=np.append(f,np.arange(101,183))
-max_dist=0.1 #11km    
-subtract_amean= True
-detrend=False
-deseasonalize=False
-resample_freq='H_mean'
-refFreqs = 0.01
-output_qnts = np.array([.05,.17,.5,.83,.95])
 
+#### 1. Get & manipulate SLR projections (to be loaded from cfg)
+slr = xr.open_mfdataset(("/Volumes/Naamloos/PhD_Data/AR6_projections/ar6-GESLA3-samples-total/wf_1f/ssp585/per_tg/Fort_Hamilton_New_York_1.nc",
+                         "/Volumes/Naamloos/PhD_Data/AR6_projections/ar6-GESLA3-samples-total/wf_1f/ssp585/per_tg/Aberdeen.nc"),concat_dim='locations',combine='nested').load()
+#slr = xr.open_dataset(cfg['esl']['sl_projections']) #open sl projections
 
-slr = xr.open_dataset(cfg['esl']['sl_projections']) #open sl projections
-
-target_years = np.array(cfg['project_af']['target_years'].split(',')).astype('int')
-try:
-    slr = slr.sel(years=target_years) #select target years
-except:
-    raise Exception('Could not select target years from sea-level projections.')
-
-try:
+if 'locations' not in slr.dims:
     slr = slr.expand_dims('locations')
-except:
-    pass
 
 if 'samples' in slr.dims:
-    num_mc = len(slr.samples)
-else:
-    num_mc = 1
-    
-#implement options to have samples of shape/scale without slr samples 
+    if cfg['global-options']['num_mc']>len(slr.samples):
+        raise Exception('Insufficient SLR samples for desired number of Monte Carlo samples.')
+    else:
+        idx = np.sort(np.random.choice(len(slr.samples), size=cfg['global-options']['num_mc'], replace=False))
+    slr = slr.isel(samples = idx)
 
 if slr.sea_level_change.units=='mm':
     slr['sea_level_change'] = slr['sea_level_change']/1000
     slr.sea_level_change.attrs['units'] = 'm'
     
-#determine which ESL data to use
-if (cfg['esl']['input_type'] == 'raw') & (cfg['esl']['input_source'] in ['gesla2','gesla3']):
-    if cfg['esl']['input_source'] == 'gesla2':
-        (station_names, station_lats, station_lons, station_filenames) = extract_GESLA2_locations('/Volumes/Naamloos/PhD_Data/GESLA2/private_14032017_public_110292018/')
-    elif cfg['esl']['input_source'] == 'gesla3':
-        (station_names, station_lats, station_lons, station_filenames) = extract_GESLA3_locations('/Volumes/Naamloos/PhD_Data/GESLA3/GESLA3_ALL.csv')
+####
 
-    min_idx = [mindist(x,y,station_lats,station_lons, max_dist) for x,y in zip(slr.lat.values, slr.lon.values)] #sites within maximum distance from point
+#### 2. Get ESL information near queried locations
+esl_statistics = {}
 
+#derive GPD parameters from raw data
+if (cfg['esl']['input_type'] == 'raw') & (cfg['esl']['input_source'].lower() in ['gesla2','gesla3']): #if using raw data from GESLA
+    from ESL_stats_from_raw_GESLA import ESL_stats_from_raw_GESLA
+    esl_statistics = ESL_stats_from_raw_GESLA(slr,cfg)
 
-    matched_filenames = []
-    sites_with_esl = []
-    for i in np.arange(len(slr.locations)): #loop over sl-projection sites
-        if min_idx[i] is not None: #if nearby ESL data found, try to analyze that data
+elif (cfg['esl']['input_type'] == 'raw') & (cfg['esl']['input_source'].lower() not in ['gesla2','gesla3']):
+    raise Exception('Cannot (yet) analyze ESLs of configured input source.')
+
+#open pre-defined GPD parameters
+#to implement
+
+#open pre-defined return curves    
+if (cfg['esl']['input_type'] == 'return_curves') & (cfg['esl']['input_source']=='COAST-RP'):
+    coast_rp_coords = pd.read_pickle(os.path.join('/Volumes/Naamloos/PhD_Data/COAST_RP/COAST_RP_full_dataset','pxyn_coastal_points.xyn'))
     
-            matched_filenames.append([station_filenames[x] for x in min_idx[i]]) #esl stations within distance to sea-level projection site
-            sites_with_esl.append(float(slr.locations[i])) #sea-level projections sites with nearby ESL data
+    min_idx = [mindist(x,y,coast_rp_coords['lat'].values,coast_rp_coords['lon'].values, 0.1) for x,y in zip(slr.lat.values, slr.lon.values)]
     
-    # if no locations with esl data are found, quit with error
-    if not matched_filenames:
-        raise Exception("No matches found within {} degrees for provided lat/lon list".format(max_dist))
-      
-    # Initialize station data dictionary to hold matched location data
-    esl_params = {}
-    	
-    # Initialize variables to track the files that have been tested
-    pass_files = {}
-    fail_files = []
-    	
-    # Loop over the matched files
-    for i in np.arange(len(sites_with_esl)): #loop over sea-level projection sites with matched ESL data
-		
-        # This ID
-        this_id = sites_with_esl[i]
-        this_id_passed = False
-		
-        # Loop over the esl files within the match radius for this location, use the first one for which data fulfills criteria
-        for esl_file in matched_filenames[i]:
-			
-            # if this esl file was already tested and passed
-            if np.isin(esl_file, list(pass_files.keys())):
-                print("{0} already PASSED a previous check on data constraints. Mapping site ID {1} to site ID {2}.".format(esl_file, pass_files[esl_file], this_id))
-                esl_params[this_id] = esl_params[pass_files[esl_file]]
-                this_id_passed = True
-
-            # if current sea-level projectio nlocation already has esl parameters, skip to the next
-            if this_id_passed:
-                continue            
-
-            # This esl file has not been tested yet, try to analyze it
-            try:
-                #this tide gauge data
-                if cfg['esl']['input_source'] == 'gesla2':
-                    dfs = open_GESLA2_files('/Volumes/Naamloos/PhD_Data/GESLA2/private_14032017_public_110292018/',
-                                        'H_mean',min_yrs=20,fns=[esl_file])
-
-                elif cfg['esl']['input_source'] == 'gesla3':
-                    dfs = open_GESLA3_files('/Volumes/Naamloos/PhD_Data/GESLA3/GESLA3.0_ALL','/Volumes/Naamloos/PhD_Data/GESLA3/GESLA3_ALL.csv',
-                                        types=['Coastal'],resample_freq = 'D_max',min_yrs = 20,fns=[esl_file])
-
-                #preproc options:
-                if detrend:
-                    dfs = detrend_gesla_dfs(dfs)
-                if deseasonalize:
-                    dfs = deseasonalize_gesla_dfs(dfs)
-                if subtract_amean:
-                    dfs = subtract_amean_from_gesla_dfs(dfs)
-
-                extremes = pot_extremes_from_gesla_dfs(dfs,99.7,declus_method='iterative_descending',declus_window=3)
-                gpd_params = fit_gpd_to_gesla_extremes(extremes)
-                esl_params[this_id] = gpd_params
-			
-                # This file passed, add it to the pass file dictionary
-                pass_files[esl_file] = this_id
-                # This ID has data, set the flag to move onto the next ID
-                this_id_passed = True
-			
-            except:
-                # This file failed, add it to the fail list and continue with the next file
-                print("{} did not pass the data constraints. Moving on to the next file.".format(esl_file))
-                fail_files.append(esl_file)
-                continue
-
-        # Let the user know we didn't find a file that passes the data constraints
-        if not this_id_passed:
-            print("No locations within {0} degrees pass the data constraints for ID {1}.".format(max_dist, this_id))
+    for i in np.arange(len(slr.locations)):
+        this_id = str(int(coast_rp_coords.iloc[min_idx[0][0]].name))
+        rc = pd.read_pickle(os.path.join('/Volumes/Naamloos/PhD_Data/COAST_RP/COAST_RP_full_dataset','rp_full_empirical_station_'+this_id+'.pkl'))
+        rc = rc['rp'][np.isfinite(rc['rp'])]
+        
+        esl_statistics[slr.locations.values[i]] = {}
+        esl_statistics[slr.locations.values[i]]['z_hist'] = rc.index.values
+        esl_statistics[slr.locations.values[i]]['f_hist'] = 1/rc.values
+    
+elif (cfg['esl']['input_type'] == 'return_curves') & (cfg['esl']['input_source']!='COAST-RP'):
+    raise Exception('Cannot (yet) open return curves of of configured input source.')    
+#### 
+        
 	
-#projection stage
-for site_id,gpd in esl_params.items():
-    if np.isscalar(refFreqs):
-        refFreq = refFreqs
+#### 3. Do the projections
+#determine reference frequencies
+if cfg['projecting']['refFreqs'] == 'diva': #find contemporary DIVA flood protection levels
+    qlats = [slr.lat.sel(locations=k).values for k in esl_statistics.keys()]
+    qlons = [slr.lon.sel(locations=k).values for k in esl_statistics.keys()]
+    
+    from find_diva_protection_levels import find_diva_protection_levels
+    
+    refFreqs = find_diva_protection_levels(qlons,qlats,"/Users/timhermans/Documents/Data/DIVA/cls.gpkg",15)
+
+elif cfg['projecting']['refFreqs'] == 'flopros': #find contemporary FLOPROS flood protection levels
+    qlats = [slr.lat.sel(locations=k).values for k in esl_statistics.keys()]
+    qlons = [slr.lon.sel(locations=k).values for k in esl_statistics.keys()]
+    
+    from find_flopros_protection_levels import find_flopros_protection_levels
+    
+    refFreqs = find_flopros_protection_levels(qlons,qlats,'/Users/timhermans/Documents/Data/FLOPROS/Tiggeloven/',15)    
+
+else:
+    if ~np.isscalar(refFreqs):
+        raise Exception('Reference frequency must be DIVA, FLOPROS or a scalar value.')
+    else:
+        refFreqs = np.tile(refFreqs,len(esl_statistics))
+
+#project amplification factors relative to reference frequencies     
+out_qnts = np.array(cfg['output']['quantiles'].split(',')).astype('float')
+   
+if cfg['output']['project_AFs']: #if projecting amplification factors
+    target_years = np.array(str(cfg['output']['AF']['target_years']).split(',')).astype('int')
+    
+    try:
+        slr = slr.sel(years=target_years) #select target years
+    except:
+        raise Exception('Could not select target years from sea-level projections.')
+
+    #loop over queried sites with required information:
+    i=0
+    output = []
+    for site_id,stats in esl_statistics.items():
+        refFreq = refFreqs[i]
+            
+        if cfg['esl']['input_type'] == 'raw': #generate return curves
+            scale_samples,shape_samples = multivariate_normal_gpd_samples_from_covmat(stats['scale'].iloc[0],stats['shape'].iloc[0],stats['cov'].iloc[0],cfg['global-options']['num_mc'])
+            z_hist = get_return_curve(f,scale_samples,shape_samples,stats['loc'].iloc[0],stats['avg_extr_pyear'].iloc[0],'mhhw',stats['mhhw'].iloc[0])
+            f_ = f
+            
+        elif cfg['esl']['input_type'] == 'return_curves': #load in return curves
+            z_hist = stats['z_hist']
+            f_ = stats['f_hist']
         
-    scale_samples,shape_samples = multivariate_normal_gpd_samples_from_covmat(gpd['scale'].iloc[0],gpd['shape'].iloc[0],gpd['cov'].iloc[0],num_mc)
-    z_hist = get_return_curve(f,scale_samples,shape_samples,gpd['loc'].iloc[0],gpd['avg_extr_pyear'].iloc[0],'mhhw',gpd['mhhw'].iloc[0])
-    for yr in target_years:
-        z_hist,z_fut,AF = compute_AF(f,z_hist,slr.sel(locations=site_id).sel(years=yr).sea_level_change.values,refFreq) #samples
         
-        #to-do: store outputs
+        for yr in target_years:
+            site_output = []
+            
+            z_hist,z_fut,AF = compute_AF(f_,z_hist,slr.sel(locations=int(site_id)).sel(years=yr).sea_level_change.values,refFreq) #samples
+        
+            #to-do: store outputs
+            ds = xr.Dataset(
+                    data_vars=dict(
+                        z_hist=(["qnt","f"], np.quantile(z_hist,q=out_qnts,axis=-1)),
+                        z_fut=(["qnt",'f'], np.quantile(z_fut,q=out_qnts,axis=-1)),
+                        AF=(["qnt"],np.quantile(AF,out_qnts))
+                    ),
+                    coords=dict(
+                        f=(["f"], f),
+                        qnt=(["qnt"], out_qnts),
+                        year=yr,
+                        site=site_id,
+                    ),
+                )
+            site_output.append(ds)
+        output.append(xr.concat(site_output,dim='year'))
+        i+=1
+    output_ds = xr.concat(output,dim='site')
+    output_ds.attrs['config'] = cfg
+    output_ds['lat'] = ('site',[slr.lat.sel(locations=k).values for k in output_ds.site.values])
+    output_ds['lon'] = ('site',[slr.lon.sel(locations=k).values for k in output_ds.site.values])
+    output_ds.set_coords(('lon','lat'))
