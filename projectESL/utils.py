5ddc13dc82d9e460768c614d23db305c02803780
Tim Hermans <timthjhermans@gmail.com>
2024-02-05 15:52:07 +0100

    added functions to download ar6 full sample projections & add to sites input

--- projectESL/utils.py
+++ projectESL/utils.py
@@ -1,7 +1,14 @@
+'''
+@author: Tim Hermans
+t(dot)h(dot)j(dot)hermans@uu(dot)nl
+'''
+
 import numpy as np
+import xarray as xr
+import os
 
 def angdist(lat0, lon0, lat, lon):
-	
+	'''calculate angular distance between coordinates (lat0,lon0) and (lat,lon)'''
 	# Convert the input from degrees to radians
 	(lat0, lon0) = np.radians((lat0, lon0))
 	(lat, lon) = np.radians((lat, lon))
@@ -16,7 +23,7 @@ def angdist(lat0, lon0, lat, lon):
 
 
 def mindist(qlat, qlon, lats, lons, limit=0.1):
-	
+	'''find indices of the smallest distances between (qlat,qlon) and (lats,lons) within angular distance limit "limit" '''
 	# Calculate the angular distance
 	dist = angdist(lats, lons, qlat, qlon)
 	
@@ -33,3 +40,35 @@ def mindist(qlat, qlon, lats, lons, limit=0.1):
 		min_idx = sort_idx[np.flatnonzero(dist[sort_idx] <= limit)]
 		
 		return(min_idx)
+    
+    
+def download_ar6_full_sample_projections_at_tgs(wf,ssp,out_dir): #~1.2Gb!
+    #see https://github.com/Rutgers-ESSP/IPCC-AR6-Sea-Level-Projections
+    ds_url = ('https://storage.googleapis.com/ar6-lsl-simulations-public-standard/'
+         'tide-gauges/full_sample_workflows/'+wf+'/'+ssp+'/total-workflow.zarr'
+       )
+    ds = xr.open_dataset(ds_url, engine='zarr', chunks='auto')
+    return ds.to_zarr(os.path.join(out_dir,'full_sample_total_'+wf+'_'+ssp+'.zarr'),mode='w')
+
+
+def download_ar6_full_sample_projections_gridded(wf,ssp,out_dir): #~52Gb!
+    #see https://github.com/Rutgers-ESSP/IPCC-AR6-Sea-Level-Projections
+    ds_url = ('https://storage.googleapis.com/ar6-lsl-simulations-public-standard/'
+         'gridded/full_sample_workflows/'+wf+'/'+ssp+'/total-workflow.zarr'
+       )
+    ds = xr.open_dataset(ds_url, engine='zarr', chunks='auto')
+    return ds.to_zarr(os.path.join(out_dir,'full_sample_total_'+wf+'_'+ssp+'.zarr'),mode='w')
+
+def add_ar6_full_sample_projections_to_sites(sites,slr_fn,out_dir):
+    
+    ds = xr.open_dataset(slr_fn, engine='zarr', chunks='auto')
+    ds_stacked = ds.stack(locations=['lon','lat'])
+    
+    min_idx = [np.argmin(angdist(x,y,ds_stacked.lat.values,ds_stacked.lon.values)) for x,y in zip(sites.lat.values, sites.lon.values)] #get nearest projections to sites
+    
+    ds_at_sites = ds_stacked.isel(locations=min_idx).drop('lat') #drop multiindex
+    ds_at_sites['locations']=sites['locations'].values #assign locations from sites file
+    
+    sites['sea_level_change'] = ds_at_sites['sea_level_change'] #add SLC variable to sites file
+
+    return sites
